3048d6d496bcd7cc35468e43fee65e1a2e4aff2e
Jamie Vaughn <wxv6@case.edu>
2017-08-13 12:57:36 -0400

    quick update

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,31 @@
 <template>
   <v-app light>
     <main>
+
       <v-navigation-drawer v-model='sideNav'>
         <v-list>
-          <v-list-tile>
-          <v-list-tile-action>
-            <v-icon>view_list</v-icon>
-            
-          </v-list-tile-action>
-          <v-list-tile-content>
-              View Ewes
-            </v-list-tile-content>
-            </v-list-tile>
+          <v-list-tile v-for='item in menuItems' :key='item.title'>
+            <v-list-tile-action>
+              <v-icon left>{{ item.icon }}</v-icon>
+            </v-list-tile-action>
+            <v-list-tile-content>{{ item.title }}</v-list-tile-content>
+          </v-list-tile>
         </v-list>
       </v-navigation-drawer>
+
       <v-toolbar dark class='primary'>
         <v-toolbar-title>Flock Roster</v-toolbar-title>
-        <v-toolbar-side-icon @click.native.stop='sideNav = !sideNav' class='hidden-sm-and-up'></v-toolbar-side-icon>
+        <v-toolbar-side-icon @click.native.stop = "sideNav = !sideNav" class='hidden-sm-and-up'>
+        </v-toolbar-side-icon>
+
         <v-spacer></v-spacer>
         <v-toolbar-items class='hidden-xs-only'>
-          <v-btn flat><v-icon dark left>view_list</v-icon>Ewes</v-btn>
-        </v-toolbar-items>    
-        
+          <v-btn flat v-for='item in menuItems' :key='item.title'>
+            <v-icon dark left>{{ item.icon }}</v-icon>{{item.title }}</v-btn>
+        </v-toolbar-items>
+
       </v-toolbar>
+
     </main>
   </v-app>
 </template>
@@ -31,12 +34,22 @@
   export default {
     data () {
       return {
-        sideNav: false
+        sideNav: false,
+        menuItems: [
+          {icons: 'supervisor_account', title: 'View Stock'},
+          {icons: 'room', title: 'New Entry'},
+          {icons: 'person', title: 'Profile'},
+          {icons: 'face', title: 'Sign up'},
+          {icons: 'lock_open', title: 'Sign in'}
+        ]
       }
     }
   }
+
 </script>
 
 <style lang="stylus">
   @import './stylus/main'
+
 </style>
+

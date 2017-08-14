45026c7a8657af2ea3187f157e0980844aabf354
Jamie Vaughn <wxv6@case.edu>
2017-08-13 20:30:09 -0400

    Update Vue Version, Fix icon display

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,16 @@
 <template>
   <v-app light>
-    <main>
-
-      <v-navigation-drawer v-model='sideNav'>
+      <v-navigation-drawer v-model='sideNav' @click.stop = "sideNav = !sideNav">
         <v-list>
-          <v-list-tile v-for='item in menuItems' :key='item.title'>
+          <v-list-tile>
+          <v-list-tile-action>
+            <v-icon left>keyboard_arrow_left </v-icon>
+            </v-list-tile-action>
+            <v-list-tile-content @click.stop = "sideNav = !sideNav">
+              Close Navigation Panel
+            </v-list-tile-content>
+          </v-list-tile>
+          <v-list-tile v-for='item in menuItems' v-bind:key='item.title'>
             <v-list-tile-action>
               <v-icon left>{{ item.icon }}</v-icon>
             </v-list-tile-action>
@@ -14,18 +20,16 @@
       </v-navigation-drawer>
 
       <v-toolbar dark class='primary'>
+        <v-toolbar-side-icon @click.stop = "sideNav = !sideNav" class='hidden-sm-and-up'></v-toolbar-side-icon>
         <v-toolbar-title>Flock Roster</v-toolbar-title>
-        <v-toolbar-side-icon @click.native.stop = "sideNav = !sideNav" class='hidden-sm-and-up'>
-        </v-toolbar-side-icon>
-
         <v-spacer></v-spacer>
         <v-toolbar-items class='hidden-xs-only'>
-          <v-btn flat v-for='item in menuItems' :key='item.title'>
+          <v-btn flat v-for='item in menuItems' v-bind:key='item.title'>
             <v-icon dark left>{{ item.icon }}</v-icon>{{item.title }}</v-btn>
         </v-toolbar-items>
-
       </v-toolbar>
-
+      
+    <main>
     </main>
   </v-app>
 </template>
@@ -36,11 +40,11 @@
       return {
         sideNav: false,
         menuItems: [
-          {icons: 'supervisor_account', title: 'View Stock'},
-          {icons: 'room', title: 'New Entry'},
-          {icons: 'person', title: 'Profile'},
-          {icons: 'face', title: 'Sign up'},
-          {icons: 'lock_open', title: 'Sign in'}
+          {icon: 'supervisor_account', title: 'View Stock'},
+          {icon: 'room', title: 'New Entry'},
+          {icon: 'person', title: 'Profile'},
+          {icon: 'face', title: 'Sign up'},
+          {icon: 'lock_open', title: 'Sign in'}
         ]
       }
     }
